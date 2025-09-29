f06dde7de782958166735d2b2fc2c2b9102be0db
samarth <samarthk007x@gmail.com>
2025-09-29 15:56:28 +0530

    demo 1

--- src/App.py
+++ src/App.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1I've deployed a change, but I've temporarily set a very strict sustainability policy for this demo.</h1>"
+    return "<h1>This is just testing .</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
+
