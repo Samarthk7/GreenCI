4c6967cac0e6781b733e13dbdf49d4af1518de85
samarth <samarthk007x@gmail.com>
2025-12-07 19:45:05 +0530

    checking if AI gatekeeper works

--- src/App.py
+++ src/App.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>This is just testing .</h1>"
+    return "<h1>Testing the AI gatekeepre.</h1>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
